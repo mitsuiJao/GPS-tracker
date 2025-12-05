4c05f1a4dcd02ea92bc44c4405e85f2456ffdfc3
mitsuijao <nishima.kun@gmail.com>
2025-12-05 11:53:52 +0900

    update: worker minecraft

--- server/worker/src/main.py
+++ server/worker/src/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello docker!")
+from paho.mqtt import client as mqtt
 
-if __name__ == "__main__":
-    # main()
-    pass
+HOST = "broker.emqx.io"
+TOPICK = "test/mitsuijao/24"
+PORT = 1883
+
+def on_connect(client, userdata, flg, rc, prop):
+    print("connect")
+    client.subscribe(TOPICK)
+
+def on_desconnect(client, userdata, rc, prop):
+    if rc != 0:
+        print("discconect")
+        
+def on_message(client, userdata, msg):
+    print(msg)
+
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_disconnect = on_desconnect
+client.on_message = on_message
+
+client.connect(HOST, PORT)
+client.loop_forever()
