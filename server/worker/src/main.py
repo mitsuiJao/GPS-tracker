a963a66dea68c8cd89b0a62da9edc5241d9baa83
mitsuijao <nishima.kun@gmail.com>
2025-12-05 12:48:43 +0900

    udpate: mqtt connected!

--- server/worker/src/main.py
+++ server/worker/src/main.py
@@ -1,23 +1,25 @@
 from paho.mqtt import client as mqtt
 
+print("working!")
 HOST = "broker.emqx.io"
-TOPICK = "test/mitsuijao/24"
+TOPIC = "test/mitsuijao/24"
 PORT = 1883
 
 def on_connect(client, userdata, flg, rc, prop):
-    print("connect")
-    client.subscribe(TOPICK)
+    print(f"Connected with result code {rc}")
+    client.subscribe(TOPIC)
+    print(f"Subscribed to topic: {TOPIC}")
 
-def on_desconnect(client, userdata, rc, prop):
+def on_disconnect(client, userdata, rc, prop):
     if rc != 0:
-        print("discconect")
+        print(f"Unexpected disconnection with result code {rc}")
         
 def on_message(client, userdata, msg):
-    print(msg)
+    print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
 
 client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
-client.on_disconnect = on_desconnect
+client.on_disconnect = on_disconnect
 client.on_message = on_message
 
 client.connect(HOST, PORT)
