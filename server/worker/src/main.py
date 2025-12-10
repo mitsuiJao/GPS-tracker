84c1ebbb6bfa6cecdc9c3a8b7981b03b0eb8eaad
mitsuijao <nishima.kun@gmail.com>
2025-12-11 01:21:41 +0900

    refactor: worker modulize db_connect.py

--- server/worker/src/main.py
+++ server/worker/src/main.py
@@ -3,6 +3,7 @@ import psycopg2
 from psycopg2 import pool
 import os
 import struct
+from db_connect import DB
 
 print("working!")
 
@@ -10,12 +11,12 @@ MQTTHOST = os.getenv("MQTT_BROKER")
 MQTTPORT = 1883
 TOPIC = "test/mitsuijao/24"
 
-DBHOST = os.getenv("DB_HOST", "localhost")
-DBPORT = 5432
-USER = "nishima"
-PASS = "nishima"
-DBNAME = "biketracker"
-TABLE = "coordinate"
+# DBHOST = os.getenv("DB_HOST", "localhost")
+# DBPORT = 5432
+# USER = "nishima"
+# PASS = "nishima"
+# DBNAME = "biketracker"
+# TABLE = "coordinate"
 
 db_pool = None
 
@@ -32,43 +33,42 @@ def on_message(client, userdata, msg):
     payload = msg.payload.decode()
     print(f"Received message on topic {msg.topic}: {payload}")
     latitude, longitude = payload.split(",")
-    save_db(float(latitude), float(longitude))
+    DB.insertDB(float(latitude), float(longitude))
 
 
-def init_pool():
-    global db_pool
-    try:
-        db_pool = pool.SimpleConnectionPool(
-            minconn=1,
-            maxconn=5,
-            host=DBHOST,
-            port=DBPORT,
-            dbname=DBNAME,
-            user=USER,
-            password=PASS
-        )
-    except psycopg2.Error as e:
-        print(f"DB connection error: {e}")
-        raise
+# def init_pool():
+#     global db_pool
+#     try:
+#         db_pool = pool.SimpleConnectionPool(
+#             minconn=1,
+#             maxconn=5,
+#             host=DBHOST,
+#             port=DBPORT,
+#             dbname=DBNAME,
+#             user=USER,
+#             password=PASS
+#         )
+#     except psycopg2.Error as e:
+#         print(f"DB connection error: {e}")
+#         raise
 
-def save_db(latitude, longitude):
-    conn = db_pool.getconn()
+# def save_db(latitude, longitude):
+#     conn = db_pool.getconn()
     
-    try:
-        cur = conn.cursor()
-        sql = f"INSERT INTO {TABLE} (timestamp, latitude, longitude) VALUES (NOW(), %s, %s)"
-        cur.execute(sql, (latitude, longitude))
-        conn.commit()
+#     try:
+#         cur = conn.cursor()
+#         sql = f"INSERT INTO {TABLE} (timestamp, latitude, longitude) VALUES (NOW(), %s, %s)"
+#         cur.execute(sql, (latitude, longitude))
+#         conn.commit()
 
-    except Exception as e:
-        conn.rollback()
-        print(f"DBerror: {e}")
-    finally:
-        db_pool.putconn(conn)
+#     except Exception as e:
+#         conn.rollback()
+#         print(f"DBerror: {e}")
+#     finally:
+#         db_pool.putconn(conn)
 
 def main():
-    init_pool()
-
+    # init_pool()
     client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
