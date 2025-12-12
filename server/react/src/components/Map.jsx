19ec50bbcf09718af4dbf338a87eb409e2602fc5
mitsuijao <nishima.kun@gmail.com>
2025-12-13 02:57:58 +0900

    update: google map api

---
+++ server/react/src/components/Map.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { GoogleMap, LoadScript, Polyline } from "@react-google-maps/api";
+import axios from "axios";
+
+const containerStyle = {
+    height: "100%",
+    width: "100%",
+};
+
+const center = {
+    lat: 35.69575,
+    lng: 139.77521,
+};
+
+const flightPath = [
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+];
+
+const polylineOptions = {
+    strokeColor: '#FF0000', 
+    strokeOpacity: 1.0,     
+    strokeWeight: 4,        
+    geodesic: true,         
+};
+
+const key = import.meta.env.VITE_GCP_APIKEY;
+
+function Map() {
+    return ( // マップの位置おかしいのとサイズ調整しといて
+        <LoadScript googleMapsApiKey={key}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={17}
+            > 
+                <Polyline
+                    path={flightPath}
+                    options={polylineOptions}
+                />
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default Map;
---
+++ server/react/src/pages/MapMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../components/Map";
+
+function MapMain() {
+    return (
+        <div style={{ width: "100%", height:"100%" }}>
+            <Map />
+        </div>
+    );
+}
+
+export default MapMain;
